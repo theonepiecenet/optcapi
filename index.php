1ad4f6ef12c0a9c5a5e9a22093cf512b4b795859
root <root@ip-172-31-25-160.ap-northeast-2.compute.internal>
2018-06-20 12:34:06 +0000

    add tags.json name.json

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require './vendor/autoload.php';
 $app = new \Slim\App;
 $container = $app->getContainer();
 $app->get('/', function ($req, $res, $args=[]) {
-    echo "theonepiece.net api server";
+    echo "This is theonepiece.net api server";
+    echo "please visit this repository, <a href='https://github.com/theonepiecenet/optcapi'>theonepiecenet/optcapi</a>";
 });
 
 $app->get('/{lang}/character/{id}', function($request, $response) {
@@ -13,24 +14,91 @@ $app->get('/{lang}/character/{id}', function($request, $response) {
     $original_file = "res/en/character/".$id;
     $local_file = "res/".$lang."/character/".$id;
     $original_json = getFile($original_file);
-    $original_arr = json_decode($original_json, true);
-
-    if($lang != "en")
+    if($original_json == 404)
     {
-        $local_json = getFile($local_file);
-        $local_arr  = json_decode($local_json, true);
-        $local_keys = array_keys($local_arr);
-    
-        for($i=0;$i<count($local_arr);$i++)
-	{
-            $original_arr[$local_keys[$i]] = $local_arr[$local_keys[$i]];
-	}
+        header("HTTP/1.0 404 Not Found");
+        exit();
     }
-    $json = json_encode($original_arr, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
-    $response->getBody()->write($json);
+    else
+    {
+        $original_arr = json_decode($original_json, true);
+
+        if($lang != "en")
+        {
+            $local_json = getFile($local_file);
+            $local_arr  = json_decode($local_json, true);
+            $local_keys = array_keys($local_arr);
+        
+            for($i=0;$i<count($local_arr);$i++)
+        {
+                $original_arr[$local_keys[$i]] = $local_arr[$local_keys[$i]];
+        }
+        }
+        $json = json_encode($original_arr, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        $response->getBody()->write($json);
+    }
+    
     return $response;
 });
 
+$app->post('/{lang}/search', function($request, $response) {
+    $result_arr = [];
+    $result_name = [];
+    $lang = $request->getAttribute('lang');
+    $allPostPutVars = $request->getParsedBody();
+    foreach($allPostPutVars as $key => $param){
+        if($key == "name")
+        {
+            $value = $param;
+            $fp = fopen("res/".$lang."/name.json", "r");
+            $name = fread($fp, filesize("res/".$lang."/name.json"));
+            $name = json_decode($name, true);
+            fclose($fp);
+            for($i=0; $i<count($name);$i++)
+            {
+                if(array_search(strtolower($value), array_map('strtolower', $name[$i]['aliases'])) > -1)
+                {
+                    array_push($result_name, $name[$i]['id']);
+                }
+            }
+        }
+        elseif($key == "captain" || $key == "sailor" || $key == "special" || $key == "limit")
+        {
+            $value = json_decode($param);
+            $fp = fopen("res/".$lang."/tags.json", "r");
+            $tag= fread($fp, filesize("res/".$lang."/tags.json"));
+            $tag= json_decode($tag, true);
+            fclose($fp);
+	    for($i=0; $i<count($tag);$i++)
+	    {
+		if($tag[$i]['match'] == $key)
+		{
+                    for($j=0; $j<count($value);$j++)
+                    {
+                        if($tag[$i]['tag'] == $value[$j])
+                        {
+                            if(count($result_arr) > 0)
+		                $result_arr = array_intersect($result_arr,$tag[$i]['target']);
+                            else
+                                $result_arr = $tag[$i]['target'];
+                        }
+                    }
+		}
+	    }
+        }
+    }
+    if(count($result_arr) > 0)
+        $result_arr = array_values($result_arr);
+
+    if(count($result_name) > 0 && count($result_arr) > 0) {
+        $result_arr = array_values(array_intersect($result_arr, $result_name));
+    }
+    elseif(count($result_name) > 0 && count($result_arr) == 0)
+        $result_arr = $result_name;
+
+    $response->getBody()->write(json_encode($result_arr));
+    return $response;
+});
 $app->run();
 
 function getFile($path)
@@ -41,6 +109,16 @@ function getFile($path)
         $json = fread($fp, filesize($path));
         fclose($fp);
     }
+    else
+    {
+        $json = 404;
+    }
     return $json;
+}
+function objectToArray ($object) {
+    if(!is_object($object) && !is_array($object))
+        return $object;
+
+    return array_map('objectToArray', (array) $object);
 }
 ?>
